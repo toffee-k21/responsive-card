0243fbf424a66539895b5f09c9ba88e89f5e34e4
Taufiq <taufiq9.2004@gmail.com>
2023-11-18 14:55:46 +0530

    responsive card

---
+++ t21.js
@@ -0,0 +1,20 @@
+let see = document.querySelector('.see')
+let text = document.querySelector('.text')
+// let card = document.querySelector('.card')
+
+// fun=()=>{
+//     if(text.style.height == '50px'){
+//         text.style.height = '600px'
+//     }
+//     else if(text.style.height == '600px'){
+//         text.style.height == '50px'
+//     }
+//     else{
+//         text.style.height == '50px'
+//     }
+// }
+
+function oss(){
+    text.classList.toggle('text-alt')
+    text.classList.toggle('text')
+}
